11788a402972b7c3d027ae86ed324172c8d57cab
Jun Hayakawa <30480420+jun0207@users.noreply.github.com>
2022-06-29 18:21:34 +0900

    Update main.cc

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@ int main() {
     std::cout << "Test Program: " << std::to_string(test_val) << std::endl;
     char c = 200;
     int i = 1000;
+    int a = 1;
     printf("i/c = %d\n", i/c);
     // what is the a value??/
     a++;
